ae751ac8e9f3ad3568202367a8139a8c9800970e
kerrishotts@gmail.com <kerrishotts@Kerris-Macbook-Pro.local>
2016-12-30 15:16:35 -0600

    abstract a little bit
    devices are passed in by caller, as are any ROMs
    can be used without a screen as well

--- js/Computer.js
+++ js/Computer.js
@@ -8,7 +8,6 @@ import Keyboard from "./devices/Keyboard.js";
 import memoryLayout from "./memoryLayout.js";
 import log from "./log.js";
 
-import font from "../design/font0.js";
 
 const TRAPS = {
     FRAME: 0xF0,
@@ -19,6 +18,8 @@ export default class Computer {
 
     constructor({
         screenId = "screen",
+        devices = [],
+        roms = [],
         beforeFrameUpdate,
         debug
     } = {}) {
@@ -34,113 +35,148 @@ export default class Computer {
         this.memory = new Memory(memoryLayout);
         this.memory.init();
 
-        this.io = new IO();
-
-        this.devices = {
-            keyboard: new Keyboard(this.io),
-        }
-
-        this.cpu = new CPU(this.memory, this.io);
-
-        // simulate ROM font for now
-        var tileSet0Offset = memoryLayout.tileSet0;
-        font.forEach((v, i) => {
-            this.memory.poke(i+tileSet0Offset, v);
+        // load ROMS
+        this.roms = roms;
+        this.roms.forEach(rom => {
+            this.memory.loadFromJS(rom);
         });
 
-        this.screen = new Screen(screenId, this.memory);
+        this.io = new IO();
+        this.cpu = new CPU(this.memory, this.io);
 
-        this.rafId = undefined;
+        this.devices = {};
+
+        devices.forEach(Device => {
+            let device = new Device({ io: this.io, cpu: this.cpu, memory: this.memory});
+            this.devices[device.name] = device;
+        });
+
+
+        if (screenId) {
+            this.screen = new Screen(screenId, this.memory);
+        } else {
+            this.screen = null;
+        }
+        this.beforeFrameUpdate = beforeFrameUpdate;
 
         this.resetStats();
 
         this.tick = (f) => {
-            let startTime = performance.now();
-            let deltaf = f - this.stats.oldf;
-            this.stats.oldf = f;
-            
-            // is there a beforeFrameUpdate callback? If so, call it
-            if (beforeFrameUpdate) {
-                beforeFrameUpdate(this, deltaf);
+            if (this.screen) {
+                this.tickInBrowser(f);
+            } else {
+                this.tickOutsideBrowser(f);
             }
+        }
+    }
+    tickInBrowser(f) {
+        let startTime = performance.now();
+        let deltaf = f - this.stats.oldf;
+        this.stats.oldf = f;
+        
+        // is there a beforeFrameUpdate callback? If so, call it
+        if (this.beforeFrameUpdate) {
+            this.beforeFrameUpdate(this, deltaf);
+        }
 
+        if (this.screen) {
             // update the screen
             this.screen.update();
             this.screen.draw();
+        }
 
-            // we need to know how long that took so we can devote
-            // a safe amount of time to the processor
+        // we need to know how long that took so we can devote
+        // a safe amount of time to the processor
 
-            let curTime = performance.now();
-            let deltaToNow = curTime - startTime;
-            let stopTime = curTime + (this.performance.timeToDevoteToCPU);
-            if (stopTime < performance.now()) {
-                stopTime = performance.now() + this.performance.minTimeToDevoteToCPU;
-            }
+        let curTime = performance.now();
+        let deltaToNow = curTime - startTime;
+        let stopTime = curTime + (this.performance.timeToDevoteToCPU);
+        if (stopTime < performance.now()) {
+            stopTime = performance.now() + this.performance.minTimeToDevoteToCPU;
+        }
 
-            // send a frame interrupt to the CPU
-            this.cpu.sendTrap(TRAPS.FRAME);
+        // send a frame interrupt to the CPU
+        this.cpu.sendTrap(TRAPS.FRAME);
 
-            // run the processor for awhile
-            if (this.cpu.stepping) {
-                this.stats.lastFrameInstructions = 0;
-                this.cpu.step();
-                this.stats.lastFrameInstructions++;
-                this.stats.totalInstructions++;
-            } else {
-                this.stats.lastFrameInstructions = 0;
-                if (this.cpu.running && !this.cpu.paused) {
-                    while (performance.now() < stopTime) {
-                        for (let i = this.performance.iterationsBetweenTimeCheck; i > 0; i--) {
-                            if (this.cpu.running && !this.cpu.paused) {
-                                this.cpu.step();
-                                this.stats.lastFrameInstructions++;
-                                this.stats.totalInstructions++;
-                            }
+        // run the processor for awhile
+        if (this.cpu.stepping) {
+            this.stats.lastFrameInstructions = 0;
+            this.cpu.step();
+            this.stats.lastFrameInstructions++;
+            this.stats.totalInstructions++;
+        } else {
+            this.stats.lastFrameInstructions = 0;
+            if (this.cpu.running && !this.cpu.paused) {
+                while (performance.now() < stopTime) {
+                    for (let i = this.performance.iterationsBetweenTimeCheck; i > 0; i--) {
+                        if (this.cpu.running && !this.cpu.paused) {
+                            this.cpu.step();
+                            this.stats.lastFrameInstructions++;
+                            this.stats.totalInstructions++;
                         }
                     }
                 }
             }
+        }
 
+        // compute final stats and vary performance
+        let endTime = performance.now();
+        let totalTime = endTime - startTime;
+        this.stats.lastFrameTime = totalTime;
+        this.stats.totalTime += totalTime;
+        this.stats.totalFrames ++;
+        this.stats.performanceAtTime = endTime;
 
-            // compute final stats and vary performance
-            let endTime = performance.now();
-            let totalTime = endTime - startTime;
-            this.stats.lastFrameTime = totalTime;
-            this.stats.totalTime += totalTime;
-            this.stats.totalFrames ++;
-            this.stats.performanceAtTime = endTime;
-
-            if (!this.cpu.stepping) {
-                // only throttle when stepping
-                if (totalTime > this.performance.throttlePoint) {
-                    // we need to limit processing time
-                    this.performance.timeToDevoteToCPU = Math.round((this.performance.timeToDevoteToCPU * 100) - 25) / 100;
-                    if (this.performance.timeToDevoteToCPU < this.performance.minTimeToDevoteToCPU) {
-                        this.performance.timeToDevoteToCPU = this.performance.minTimeToDevoteToCPU 
-                    }
-                } else if (totalTime < this.performance.maxTimeToDevoteToCPU) {
-                    // but we need to increase processing to use the most of the
-                    // available time as possible
-                    this.performance.timeToDevoteToCPU = Math.round((this.performance.timeToDevoteToCPU * 100) + 25) / 100;
-                    if (this.performance.timeToDevoteToCPU > this.performance.maxTimeToDevoteToCPU) {
-                        this.performance.timeToDevoteToCPU = this.performance.maxTimeToDevoteToCPU;
-                    }
+        if (!this.cpu.stepping) {
+            // only throttle when stepping
+            if (totalTime > this.performance.throttlePoint) {
+                // we need to limit processing time
+                this.performance.timeToDevoteToCPU = Math.round((this.performance.timeToDevoteToCPU * 100) - 25) / 100;
+                if (this.performance.timeToDevoteToCPU < this.performance.minTimeToDevoteToCPU) {
+                    this.performance.timeToDevoteToCPU = this.performance.minTimeToDevoteToCPU 
+                }
+            } else if (totalTime < this.performance.maxTimeToDevoteToCPU) {
+                // but we need to increase processing to use the most of the
+                // available time as possible
+                this.performance.timeToDevoteToCPU = Math.round((this.performance.timeToDevoteToCPU * 100) + 25) / 100;
+                if (this.performance.timeToDevoteToCPU > this.performance.maxTimeToDevoteToCPU) {
+                    this.performance.timeToDevoteToCPU = this.performance.maxTimeToDevoteToCPU;
                 }
             }
+        }
 
-            if (this.debug) {
-                this.cpu.dump();
-                this.memory.dump();
-                this.dump();
+        if (this.debug) {
+            this.cpu.dump();
+            this.memory.dump();
+            this.dump();
+        }
+        if (this.cpu.running && !this.cpu.stepping) {
+            window.requestAnimationFrame(this.tick);
+        }
+        if (this.cpu.stepping) {
+            this.cpu.stepping = false;
+            this.cpu.running = false;
+        }
+    }
+
+    tickOutsideBrowser(f) {
+        if (this.cpu.stepping) {
+            this.cpu.step();
+            this.stats.totalInstructions++;
+        } else {
+            if (this.cpu.running && !this.cpu.paused) {
+                this.cpu.step();
+                this.stats.totalInstructions++;
             }
-            if (this.cpu.running && !this.cpu.stepping) {
-                window.requestAnimationFrame(this.tick);
-            }
-            if (this.cpu.stepping) {
-                this.cpu.stepping = false;
-                this.cpu.running = false;
-            }
+        }
+        if (this.debug) {
+            this.cpu.dump();
+            this.memory.dump();
+            this.dump();
+        }
+        if (this.cpu.stepping) {
+            this.cpu.stepping = false;
+            this.cpu.running = false;
         }
     }
 
@@ -216,10 +252,9 @@ export default class Computer {
         this.memory.init();
         this.screen.init();
 
-        // simulate ROM font for now
-        var tileSet0Offset = memoryLayout.tileSet0;
-        font.forEach((v, i) => {
-            this.memory.poke(i+tileSet0Offset, v);
+        // reload ROMs
+        this.roms.forEach(rom => {
+            this.memory.loadFromJS(rom);
         });
 
         //this.reset();
