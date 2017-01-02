f28c5dff12d7cb3ae40f44a55b80a8118bab7ae9
kerrishotts@gmail.com <kerrishotts@Kerris-Macbook-Pro.local>
2017-01-01 20:18:21 -0600

    starting the KERNEL

---
+++ asm/kernel/cursor/cursor.asm
@@ -0,0 +1,42 @@
+.data+ 0x0B000
+#
+# cursor control
+#
+# ####################################################################
+
+.var cursor-pos
+    .dw 0x0000
+
+.var cursor-visible
+    .db 0x01
+
+.var cursor-blink-enable
+    .db 0x01
+
+.var cursor-blink-counter
+    .db 30
+
+.var cursor-blink-reset
+    .db 30
+
+
+.code+ 0x0C000
+#
+# cursor code
+#
+# ####################################################################
+
+cursor-render:
+    PUSHA
+    XOR A, A
+    LDI AL, bank(&cursor-visible)
+    MOV DB, A
+    LDI A, [addr(&cursor-visible)]
+    IF Z
+        BR :cursor-render-exit
+
+    
+    
+cursor-render-exit:
+    POPA
+    RET
---
+++ asm/kernel/kernel.asm
@@ -0,0 +1,11 @@
+#
+# 6516 Retroputer KERNEL
+#
+
+.data 0x0B000
+
+.code 0x0C000
+
+.import cursor/cursor.asm
+
+
