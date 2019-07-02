d738aac4a1ceb9dbbdbf413755346ea173790288
Kerri Shotts <kerrishotts@gmail.com>
2019-07-01 22:37:03 -0400

    Closer to a computer :-)

--- src/core/Processor.js
+++ src/core/Processor.js
@@ -6,6 +6,7 @@ import { IOBus } from "./IOBus.js";
 import { RegisterFile } from "./RegisterFile.js";
 import { decodeInstruction } from "../isa/decodeInstruction.js";
 import { TASK_FNS } from "../isa/tasks.js";
+import { decode } from "punycode";
 
 const _alu = Symbol("_alu");
 const _registerFile = Symbol("_registerFile");
@@ -19,6 +20,8 @@ const _taskQueue = Symbol("_taskQueue");
 const _stack = Symbol("_stack");
 const _cache = Symbol("_cache")
 
+const MAX_CACHE = 256;
+const MAX_TASKS = 256;
 
 export class Processor {
 
@@ -35,6 +38,7 @@ export class Processor {
 
         this[_memory] = memory;
         this[_systemBus] = systemBus;
+        this[_systemBus].map = 0b0000110001000001; // banks 1, 2, 3
         this[_ioBus] = ioBus;
         this[_clock] = clock;
 
@@ -88,6 +92,16 @@ export class Processor {
      */
     get clock() { return this[_clock]; }
 
+    get internalState() {
+        return {
+            pc: this.registers.PC,
+            mp: this.registers.MP,
+            cache: this[_cache],
+            stack: this[_stack],
+            tasks: this[_taskQueue]
+        };
+    }
+
     /**
      * Injects bytes into the fetch cache
      *
@@ -95,53 +109,83 @@ export class Processor {
      * @returns {void}
      * @memberof Processor
      */
-    inject(bytes) {
-        this[_cache].push(...bytes);
+    inject(addr, bytes) {
+        this[_cache].push(...bytes.map((byte, idx) => [addr+idx, byte]));
     }
 
     _fetch() {
-        const byte = this.memory.readByte(this.registers.MP++);
-        this.inject([byte]);
+        const byte = this.memory.readByte(this.registers.MP);
+        if (byte === undefined) {
+            throw new Error("Bad memory");
+        }
+        this.inject(this.registers.MP, [byte]);
+        this.registers.MP += 1;
     }
 
     _decode() {
-        for (let i = 1; i <= 4; i++) {
-            const tasks = decodeInstruction(this[_cache].slice(0,i));
-            if (tasks) {
-                this[_cache] = this[_cache].slice(i+1);
-                this.registers.PC += i;
-                this[_taskQueue].push(...tasks);
+        let pc = this[_cache][0] && this[_cache][0][0];
+        const bytes = this[_cache].map(([_, byte]) => byte);
+        const preDecodeLength = bytes.length;
+        const tasks = decodeInstruction(bytes);
+        if (tasks) {
+            const postDecodeLength = bytes.length;
+            const decodedInstructionLength = preDecodeLength - postDecodeLength;
+
+            // remove the decoded instruction from the cache
+            this[_cache] = this[_cache].slice(decodedInstructionLength);
+            pc += decodedInstructionLength;
+            // add the tasks to the queue, with associated PC
+            this[_taskQueue].push(...tasks.filter(task => !!task).map(task => [pc & 0xFFFF, task]));
+            return;
+        } else {
+            if (this[_cache].length > 4) {
+                // failed to decode what was in the cache, and it's
+                // long enough to have done so
+                throw new Error(`Couldn't properly decode cache: ${this[_cache]}`);
             }
         }
     }
 
     _execute() {
         if (this[_taskQueue].length > 0) {
-            const [task, operand] = this[_taskQueue].pop();
-            TASK_FNS[task]({
-                stack: this[_stack],
-                registerFile: this.registers,
-                alu: this.alu,
-                memory: this.memory,
-                args: [operand]
-            });
+            const [pc, [task, operand]] = this[_taskQueue].shift();
+            // make sure PC is set to the address of the instruction
+            // we're executing
+            this.registers.PC = pc;
+            if (task !== undefined) {
+                TASK_FNS[task]({
+                    stack: this[_stack],
+                    registerFile: this[_registerFile],
+                    alu: this[_alu],
+                    memory: this[_memory],
+                    args: [operand]
+                });
+            }
+            if (this.registers.PC !== pc) {
+                // we've jumped -- clear the cache and task queue
+                // and start fetching from the new location
+                this[_stack] = [];
+                this[_cache] = [];
+                this[_taskQueue] = [];
+                this.registers.MP = this.registers.PC;
+            }
+
         }
     }
 
     tick() {
-        if (this[_cache].length < 256) {
+        // we'll fetch up to 256 instructions / tasks
+        if (this[_cache].length < MAX_CACHE && this[_taskQueue].length < MAX_TASKS) {
             this._fetch();
         }
 
+        if (this[_cache].length > 0 && this[_taskQueue].length < MAX_TASKS) {
+            this._decode();
+        }
+
+        // if there's something in the queue, execute it :-)
         if (this[_taskQueue].length > 0) {
-            const curPC = this.registers.PC;
             this._execute();
-            if (this.registers.PC !== curPC) {
-                // we've jumped -- clear the cache
-                // and start fetching from the new location
-                this[_cache] = [];
-                this.registers.MP = this.registers.PC;
-            }
         }
     }
 }
