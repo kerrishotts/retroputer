a6d39f0d6918822d11f81c602870d3a43b4ba4b8
Kerri Shotts <kerrishotts@gmail.com>
2020-07-08 16:27:14 -0400

    Add some color and key constants

--- asm/kernel/basic/constants.asm
+++ asm/kernel/basic/constants.asm
@@ -4,6 +4,7 @@
     .const CLS 12
     .const SPACE 0x20
     .const QUOTE 0x22
+    .const CURSOR_LEFT 17
 
     .const MODE_DIRECT 0x00
     .const MODE_RUN    0x01
@@ -15,4 +16,14 @@
 
     .const EXIT_EARLY 0xFF
     .const NO_STMT_TERM_NEEDED 0xFE
+
+    .const COMMAND_COLOR  22 # Yellow
+    .const FUNCTION_COLOR 22 # Yellow
+    .const OPERATOR_COLOR 22 # Yellow
+    .const GROUPING_COLOR 23 # White
+    .const LINENUM_COLOR  23 # White
+    .const NUMBER_COLOR   19 # Cyan
+    .const STRING_COLOR   21 # Purple
+    .const VARIABLE_COLOR 23 # White
+
 }
